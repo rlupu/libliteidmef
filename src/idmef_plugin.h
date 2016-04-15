dc64ecfb62fe80a42a6a2556d16f0136b67f0dfb
rlupu <rlupu@elcom.pub.ro>
2016-04-15 13:59:46 +0300

    fixed some idents

--- src/idmef_plugin.h
+++ src/idmef_plugin.h
@@ -3,7 +3,7 @@
    the API to the IDMEF-based alerting layer.
    Copyright (C) 2015 Radu Lupu
 
-  	Libliteidmef library is free software; you can redistribute it and/or
+   Libliteidmef library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
    version 2.1 of the License, or (at your option) any later version.
